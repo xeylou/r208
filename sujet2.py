9ae1580517df650f1a4e21352ef8402c34fee58c
xeylou <71969414+xeylou@users.noreply.github.com>
2023-03-17 14:31:13 +0000

    q3 & q4

--- sujet2.py
+++ sujet2.py
@@ -1,37 +1,44 @@
 
 ## init ##
 
-tab_personnes = []  # nom, prénom, sexe, date de naissance
-positionnement_personnes = []  # tuple d'entiers d'un numéro identifiant & de la position de la personne dans tab_personnes
+personnes_famille = []  # nom, prénom, sexe, date de naissance
+liens_parentés = []     # tuple numéro enfant & parent
 
 
-def ajout_personne(nom, prenom, sexe, date_de_naissance):
-    tab_personnes.append([nom, prenom, sexe, date_de_naissance])
-    id_personne = 100 + len(tab_personnes) - 1  # identifiant suivant le dernier dans le tableau
-    position_personne = len(tab_personnes) - 1  # position dans tab_personnes
-    positionnement_personnes.append((id_personne, position_personne))
+def ajout_personne(nom, prénom, sexe, date_de_naissance):
+    informations = [nom, prénom, sexe, date_de_naissance]
+    personnes_famille.append(informations)
+
 
 def affichage_tableau(tableau: list()):
-    for ligne in tableau:
-        print(ligne, "\n")
+    for personne in tableau:
+        print(personne, "\n")
 
-def affichage_personne(numero: int()):
-    for position in positionnement_personnes:
-        if position[0] == numero:
-            position_tableau = position[1]
-            return(tab_personnes[position_tableau])
+
+def numéro_personne(nom, prénom):
+    cpt = 0
+    for personne in personnes_famille:
+        cpt += 1
+        if personne[0] == nom and personne[1] == prénom:
+            return(cpt - 1)
+
+def construction_famille(parent, enfant):
+    liens_parentés.append(parent, enfant)
+
+
+
+
 
 
 ## debug ##
 
-tab_personnes.append(["Déhu", "Alexis", "Homme", "21/12/2004"])
-tab_personnes.append(["Jean", "Jean", "Homme", "06/06/1944"])
-positionnement_personnes.append((100, 0))
-positionnement_personnes.append((101, 1))
+personnes_famille.append(["Déhu", "Laurent", "Homme", "???"])        # parent
+personnes_famille.append(["Déhu", "Alexis", "Homme", "21/12/2004"])  # enfant
+liens_parentés.append((1, None))
+liens_parentés.append((None, 0))
 
-# ajout_personne("Déhu", "Justine", "Femme", "06/01/2002")
 # ajout_personne("Didier", "Didier", "Homme", "hier")
-# print(affichage_tableau(tab_personnes))
-#print(affichage_personne(100))
+# print(affichage_tableau(personnes_famille))
+# print(numéro_personne("Déhu", "Laurent"))
 
-print("\nTableau des personnes : ", tab_personnes, "\n\nTableau des positionnements : ", positionnement_personnes)
+#print("\nTableau des personnes : ", personnes_famille, "\n\nTableau des positionnements : ", liens_parentés)
