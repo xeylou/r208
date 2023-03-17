a9831ec9b774fd4d72fe4a4465ee80049a55df09
xeylou <71969414+xeylou@users.noreply.github.com>
2023-03-17 13:51:26 +0000

    questions 1 & 2

--- sujet2.py
+++ sujet2.py
@@ -7,7 +7,11 @@ positionnement_personnes = []  # tuple d'entiers des positions de 2 personnes
 
 def ajout_personne(nom, prénom, sexe, date_de_naissance):
     tab_personnes.append([nom, prénom, sexe, date_de_naissance])
-    positionnement_personnes.append()
+
+def affichage_tableau(tableau):
+    for ligne in tableau:
+        print(ligne)
+
 
 
 ## debug ##
@@ -16,3 +20,5 @@ tab_personnes.append(["Déhu", "Alexis", "Homme", "21/12/2004"])
 tab_personnes.append(["Jean", "Jean", "Homme", "06/06/1944"])
 
 positionnement_personnes.append((0, 1))
+
+print("\nTableau des personnes : ", tab_personnes, "\n\nTableau des positionnements : ",positionnement_personnes)
