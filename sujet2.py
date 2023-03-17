14be97e1b8b8acebc77188acaefba1a0c395a3bb
xeylou <71969414+xeylou@users.noreply.github.com>
2023-03-17 15:59:53 +0000

    q8

--- sujet2.py
+++ sujet2.py
@@ -1,3 +1,13 @@
+'''
+j'ai l'habitude de travailler en anglais mais je me suis abstenu
+temps prit: environ 2h
+
+j'ai essayé de créer un maximum de variable au lieu d'utiliser
+des paramètres bruts afin de mieux comprendre le programme
+
+je suis disponible pour toute question dessus :)
+'''
+
 
 ## init ##
 
@@ -60,7 +70,7 @@ def découvrir_descendants(num_personne):
     return(temp_tab_descendants)
 
 
-# q7
+#  q7
 def découvrir_fraterie(num_personne):
     tab_parents = []
     tab_info_fraterie = []
@@ -77,6 +87,73 @@ def découvrir_fraterie(num_personne):
                 tab_info_fraterie.append(info_fraterie)
                 # récupération de ses informations pour l'ajouter au tableau
     return(tab_info_fraterie)
+
+
+# q8
+def affichage_ordre_alphabétique(tableau_numéro_personnes):
+    temp_tab_info_p = []
+    # j'estime que les numéros sont linéaires & continus (ex: 1->2->3)
+    # j'estime que personnes_famille est le tableau des informations
+    num_min = tableau_numéro_personnes[0][0]
+    num_max = num_min
+    for num in tableau_numéro_personnes:
+        for i in range(2):
+            if num[i] < num_min:
+                num_min = num[i]
+            if num[i] > num_max:
+                num_max = num[i]
+        # on récupère premier & dernier numéro
+    for i in range(num_min, num_max):
+        temp_tab_info_p.append(personnes_famille[i])
+    # toutes les informations des personnes sont stockées
+    for i in range(num_min, num_max):
+        for j in range(num_min+1, num_max):
+            p_observée = personnes_famille[i]
+            reste = personnes_famille[j]
+            if reste[0] < p_observée[0]:
+                temp_tab_info_p[i], temp_tab_info_p[j] = \
+                    temp_tab_info_p[j], temp_tab_info_p[i]
+            if reste[0] == p_observée[0] and reste[1] < p_observée[1]:
+                    temp_tab_info_p[i], temp_tab_info_p[j] = \
+                        temp_tab_info_p[j], temp_tab_info_p[i]
+            # tri alphabétique par noms puis par prénom si nom similaire
+    return(temp_tab_info_p)
+                    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    while len(tableau_numéro_personnes) > 0:
+        # pour récupérer tous les numéros, sécurité il n'y en aura plus
+        for i in range(num_min, num_max):
+            for j in range(i+1, num_max):
+                # méthode à la "tri sélection" pour voir si personnes
+                # suivantes doivent être mises avant alphabétiquement
+                p_observée = personnes_famille[i]
+                reste = personnes_famille[j]
+                if reste[0] < p_observée[0]:
+
+                 
+
+
+            
+
 
 
 
